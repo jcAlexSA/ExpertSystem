a0d54855aa1d93e09b23cf99f3e7218d4a954389
jcAlexSA <shurbinalexey@gmail.com>
2017-06-09 23:34:33 +0300

    change design

--- ExpertSystem/View/MBD_DefinitionView.xaml.cs
+++ ExpertSystem/View/MBD_DefinitionView.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace ExpertSystem
+namespace ExpertSystem.View
 {
     /// <summary>
     /// Interaction logic for MBD_Definition.xaml

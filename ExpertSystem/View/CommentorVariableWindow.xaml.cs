e5eeda1383b9eed7eb42b8cc56c605bea5e1325d
jcAlexSA <shurbinalexey@gmail.com>
2017-06-09 06:29:04 +0300

    set up buttons of create variable windows begin working on rule block

--- ExpertSystem/View/CommentorVariableWindow.xaml.cs
+++ ExpertSystem/View/CommentorVariableWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace ExpertSystem
+namespace ExpertSystem.View
 {
     /// <summary>
     /// Interaction logic for CommentorVariableWindow.xaml
